3a7100ca22b175978bcfe33da2df59673c4d082a
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-12-23 14:24:01 -0600

    Creando y consumiendo nuestra propia API

---
+++ pages/api/avo/[id].ts
@@ -0,0 +1,15 @@
+import { NextApiRequest } from "next"
+import { NextApiResponse } from "next"
+import DB from "@database"
+
+const allAvos = async (request: NextApiRequest, response: NextApiResponse) => {
+    const db =new DB()
+    const id = request.query.id
+    const entry = await db.getById(id as string)    
+ 
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'application/json')
+    response.end(JSON.stringify({data: entry}))
+}
+
+export default allAvos
